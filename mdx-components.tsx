bb7b96b286a88f181bd90bf3678118adb21afbb4
Antony <antony.antoniou18@gmail.com>
2025-05-12 12:03:56 +0100

    new mdx components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -20,54 +20,56 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       <p className="text-primary mt-2 mb-4 ml-2 font-light">{children}</p>
     ),
     Table: ({ caption, children }) => (
-        <figure>
-          <table className="overflow-hidden  my-2 rounded-lg ring-1 ring-accent/50 w-full border-collapse border-spacing-0 text-left text-sm">
-            {children}
-          </table>
-          {caption && (
-            <figcaption className="text-muted/70 mt-2 text-center text-sm">
-              {caption}
-            </figcaption>
-          )}
-        </figure>
+      <figure>
+        <table className="ring-accent/50 my-2 w-full border-collapse border-spacing-0 overflow-hidden rounded-lg text-left text-sm ring-1">
+          {children}
+        </table>
+        {caption && (
+          <figcaption className="text-muted/70 mt-2 text-center text-sm">
+            {caption}
+          </figcaption>
+        )}
+      </figure>
     ),
-    Thead: ({ children }) => (
-      <thead className="bg-section text-primary/80">
+    strong: ({ children }) => <strong className="font-bold">{children}</strong>,
+    blockquote: ({ children }) => (
+      <blockquote className="border-accent/30 text-muted bg-muted/10 m-4 rounded-md border-l-4 p-2 pb-1 pl-4 italic shadow-sm">
         {children}
-      </thead>
+      </blockquote>
     ),
-    Tbody: ({ children }) => (
-      <tbody>{children}</tbody>
+    hr: () => <hr className="text-accent/50 mx-6 mb-4" />,
+    Thead: ({ children }) => (
+      <thead className="bg-section text-primary/80">{children}</thead>
     ),
+    Tbody: ({ children }) => <tbody>{children}</tbody>,
     Tr: ({ children }) => (
-      <tr className="transition-colors even:bg-section/80 even:dark:bg-section/60">{children}</tr>
+      <tr className="even:bg-section/80 even:dark:bg-section/60 transition-colors">
+        {children}
+      </tr>
     ),
     Th: ({ children }) => (
-      <th className="px-4 py-2 font-semibold text-primary border-b border-muted/30">
+      <th className="text-primary border-muted/30 border-b px-4 py-2 font-semibold">
         {children}
       </th>
     ),
     Td: ({ children }) => (
-      <td className="px-4 py-2 text-primary/90 border-b border-muted/30">
+      <td className="text-primary/90 border-muted/30 border-b px-4 py-2">
         {children}
       </td>
     ),
     a: ({ children, href }) => (
-
-<Link
-  href={href}
-  className="relative inline-flex items-center text-accent group before:bg-accent before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-0 before:transition-[width] before:duration-200 before:content-[''] hover:before:w-full"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  {children}
-  <FiExternalLink
-    className="absolute left-full bg-background backdrop-blur-sm p-[5px] ml-1 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-md w-6 h-6 text-accent pointer-events-none"
-    aria-hidden="true"
-  />
-</Link>
-
-    
+      <Link
+        href={href}
+        className="text-accent group before:bg-accent relative inline-flex items-center before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-0 before:transition-[width] before:duration-200 before:content-[''] hover:before:w-full"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {children}
+        <FiExternalLink
+          className="bg-background text-accent pointer-events-none absolute top-1/2 left-full ml-1 h-6 w-6 -translate-y-1/2 rounded-md p-[5px] opacity-0 backdrop-blur-sm transition-opacity duration-300 group-hover:opacity-100"
+          aria-hidden="true"
+        />
+      </Link>
     ),
     ul: ({ children }) => (
       <ul className="mt-4 mb-4 ml-10 list-disc space-y-2 [&_ul]:mt-0">
